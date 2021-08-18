6d939771f4a9c8ded6281cbb1d4eb66d13dcacb7
TEqbal.techmantra <TechmantaEmaar>
2021-08-18 20:41:41 +0400

    modify

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-// const express = require('express');
-// const routes = express.Router();
-// const app = express();
+const express = require('express');
+const routes = express.Router();
+const app = express();
 
-// const server = require('http');
+const server = require('http');
 
-// routes.get('/', (req, res, next) => {
-//     res.send('Hi this is test')
-// });
+routes.get('/', (req, res, next) => {
+    res.send('Hi this is test')
+});
 
-// app.use('/api', routes); 
+app.use('/api', routes); 
 
 // app.listen(5000, () => {
 //     console.log('running...')
 // })
 
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
-});
+// const server = http.createServer((request, response) => {
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.end("Hello World!");
+// });
 
 const port = process.env.PORT || 1337;
-server.listen(port);
+app.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
