f01684e1578fe085fb637393afdade30974df769
TEqbal.techmantra <TechmantaEmaar>
2021-08-18 20:05:37 +0400

    adding config

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-const express = require('express');
-const routes = express.Router();
-const app = express();
+// const express = require('express');
+// const routes = express.Router();
+// const app = express();
 
-routes.get('/', (req, res, next) => {
-    res.send('Hi this is test')
+// const server = require('http');
+
+// routes.get('/', (req, res, next) => {
+//     res.send('Hi this is test')
+// });
+
+// app.use('/api', routes); 
+
+// app.listen(5000, () => {
+//     console.log('running...')
+// })
+
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!");
 });
 
-app.use('/api', routes); 
+const port = process.env.PORT || 1337;
+server.listen(port);
 
-app.listen(5000, () => {
-    console.log('running...')
-})
+console.log("Server running at http://localhost:%d", port);
