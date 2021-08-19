c8aeaccfda20967ebaf5bc6235e043f022c4f3ed
TEqbal.techmantra <TechmantaEmaar>
2021-08-19 11:35:40 +0400

    cleaning

--- index.js
+++ index.js
@@ -8,20 +8,7 @@ routes.get('/', (req, res, next) => {
     res.json({data: { name: 'Alex', address: 'chigagi USA' }})
 });
 
-app.use('/api', routes); 
-
-// app.listen(5000, () => {
-//     console.log('running...')
-// })
-
-// const http = require('http');
-
-// const server = http.createServer((request, response) => {
-//     response.writeHead(200, {"Content-Type": "text/plain"});
-//     response.end("Hello World!");
-// });
-
-// testing for commit
+app.use('/api', routes);  
 
 const port = process.env.PORT || 1337;
 app.listen(port);
